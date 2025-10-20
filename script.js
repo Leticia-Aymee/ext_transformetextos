23c067dd033214af4094eb3f89f1ab39e438fb1a
Leticia-Aymee <leleaymee@gmail.com>
2025-10-20 10:52:13 -0300

    Feature 1: Botão Primeira Letra Maíuscula

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Programa - Modelo 1
-window.document.addEventListener("DOMContentLoaded", function(){
-    // Botão Letra Maiúscula
-    window.document.querySelector("#btnMaiuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toUpperCase()
+window.document.addEventListener("DOMContentLoaded", function () {
+  // Botão Letra Maiúscula
+  window.document
+    .querySelector("#btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toUpperCase();
+    });
 
-    })
+  // Botão Letra Minúscula
+  window.document
+    .querySelector("#btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.toLowerCase();
+    });
 
-    // Botão Letra Minúscula
-    window.document.querySelector("#btnMinuscula").addEventListener("click", function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toLowerCase()
-
-    })
-})
+  // Botão Primeira Letra Maíuscula
+  window.document
+    .querySelector("#btnPriMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result").innerHTML = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+    });
+});
